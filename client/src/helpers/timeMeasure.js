fc7618a3e671a56faefad31e444a764c6c482e79
Ibrahim Akancay <iakancay@gmail.com>
2022-10-11 16:54:49 +0200

    timeMeasure function refactor and posts fetch desc for date

--- client/src/helpers/timeMeasure.js
+++ client/src/helpers/timeMeasure.js
@@ -1,13 +1,11 @@
 export const measureTime = (date) => {
   const minutes = (Date.now() - new Date(date)) / 60000;
-  const day = new Date(date).getDate();
-  const month = new Date(date).getMonth();
-  const year = new Date(date).getFullYear();
+
   if (Math.round(minutes) < 60) {
     return `${Math.round(minutes)} minutes ago`;
   } else if (Math.round(minutes) < 1440) {
     return `${Math.round(minutes / 60)} hours ago`;
   } else {
-    return `${day}/${month + 1}/${year}`;
+    return new Date(date).toDateString();
   }
 };
