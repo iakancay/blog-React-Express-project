c5dfc318af88bf952ad244f6824f2ae8b8aad350
Ibrahim Akancay <iakancay@gmail.com>
2023-02-02 00:01:32 +0100

    delete get in backend

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Backend is working..");
-});
+// app.get("/", (req, res) => {
+//   res.send("Backend is working..");
+// });
 app.use(express.static(path.join(__dirname, "./client/build")));
 app.get("*", function (_, res) {
   res.sendFile(
